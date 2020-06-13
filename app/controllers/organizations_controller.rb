6dffdc7794d6d0e93dfe6deafc7357f0a8f06149
Lori Boyd <lori.p.boyd@gmail.com>
2020-06-13 12:01:50 -0400

    Populates the organization controller with auth

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,62 @@
 class OrganizationsController < ApplicationController
+  before_action :authorized, only: [:persist]
+  before_action :set_org, only: [:update, :destroy]
+
+  def index
+    @organizations = Organization.all
+
+    render json: @organizations
+  end
+
+  def create
+    @organization = Organization.create(organization_params)
+    if @organization.valid?
+      infoToSaveInBox = {organization_id: @organization.id}
+      wristband = encode_token(infoToSaveInBox)
+      render json: {organization: OrganizationSerializer.new(@organization), token: wristband}
+    else
+      render json: {error: @organization.errors.full_messages}
+    end
+  end
+
+  def update
+    @organization.update(organization_params)
+
+    render json: @organization
+  end
+
+  def destroy
+    @organization.destroy
+
+    render json: { status: 'OK'  }
+  end
+
+  def org_persist
+    infoToSaveInBox = {organization_id: @organization.id}
+    wristband = encode_token(infoToSaveInBox)
+    render json: {organization: OrganizationSerializer.new(@organization), token: wristband}
+  end
+
+
+  def org_login
+    @organization = Organization.find_by(organizationname: params[:organizationname])
+    if @organization && @organization.authenticate(params[:password])
+      infoToSaveInBox = {organization_id: @organization.id}
+      wristband = encode_token(infoToSaveInBox)
+      render json: {organization: OrganizationSerializer.new(@organization), token: wristband}
+    else
+      render json: {error: ["INCORRECT EMAIL OR PASSWORD"]}
+    end
+  end
+
+  private
+
+  def set_org
+    @organization = Organization.find(params[:id])
+  end
+
+  def organization_params
+    params.permit(:name, :category, :email, :phone, :mission, :location, :image, :website, :twitter, :instagram, :facebook, :logo)
+  end
+
 end
