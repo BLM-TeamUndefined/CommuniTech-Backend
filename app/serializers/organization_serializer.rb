05528e2876e54c4f97abe3e4397764c024adc6aa
Lori Boyd <lori.p.boyd@gmail.com>
2020-06-13 12:49:00 -0400

    clean up

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,7 +1,3 @@
 class OrganizationSerializer < ActiveModel::Serializer
   attributes :id,:name,:category,:email,:phone,:mission,:location,:image,:website,:twitter,:instagram,:facebook,:logo
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> controllers
