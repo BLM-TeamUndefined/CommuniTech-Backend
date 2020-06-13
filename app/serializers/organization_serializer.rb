e07101b44825e74dd01ab2e9863c62112daa0a5d
Tcaprioli <Capriolityler@gmail.com>
2020-06-13 12:59:03 -0400

    organizations seeded

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,3 +1,3 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :id,:name,:category,:email,:phone,:mission,:location,:image,:website,:twitter,:instagram,:facebook,:logo
+  attributes :id,:name,:category,:description,:email,:phone,:mission,:location,:image,:website,:twitter,:instagram,:facebook,:logo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_024304) do
+ActiveRecord::Schema.define(version: 2020_06_13_155259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_06_13_024304) do
     t.string "logo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.string "password_digest"
   end
 
   create_table "professionals", force: :cascade do |t|
